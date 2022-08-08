ce6e2ccecef16f77af13fb2a60f360f4b2641432
Horik <hr.mail.qaq@gmail.com>
2022-08-08 22:45:05 +0800

    [impl] binaryInst的循环不变量提升

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -8,23 +8,25 @@ import pass.passes.*;
 import java.util.ArrayList;
 
 public class PassManager {
-    public static ArrayList<Pass> passes=new ArrayList<>();
-    public static ArrayList<MCPass> MCPasses=new ArrayList<>();
-    //官方测例可以保证不会出现undef的情况，因此某些情况下可以激进地忽略掉undef（可能无法通过中端测试）
-    public static boolean ignoreUndef=false;
+    public static ArrayList<Pass> passes = new ArrayList<>();
+    public static ArrayList<MCPass> MCPasses = new ArrayList<>();
+    // 官方测例可以保证不会出现undef的情况，因此某些情况下可以激进地忽略掉undef（可能无法通过中端测试）
+    public static boolean ignoreUndef = false;
 
     /**
      * 初始化，在此处按照顺序添加IR pass
      */
-    public static void initialization(){
+    public static void initialization() {
         passes.add(new InterproceduralAnalysis());
         passes.add(new DeadCodeEmit());
         passes.add(new Mem2Reg());
         passes.add(new GVNGCM());
 
-//        passes.add(new FuncInline());
-        passes.add(new GlobalVariableOpt());//FuncInline为其创造更多机会
-        passes.add(new Mem2Reg());//处理掉新产生的alloca
+        passes.add(new LoopInfoUpdate()); // 计算循环信息
+        passes.add(new LICM());// 循环不变量外提
+        // passes.add(new FuncInline());
+        passes.add(new GlobalVariableOpt());// FuncInline为其创造更多机会
+        passes.add(new Mem2Reg());// 处理掉新产生的alloca
         passes.add(new SimplifyCFG());
 
         passes.add(new InterproceduralAnalysis());
@@ -36,17 +38,17 @@ public class PassManager {
     /**
      * 初始化，在此处按照顺序添加MC pass
      */
-    public static void initializationMC(){
-        //MC Pass
-//        MCPasses.add(new Hello());
+    public static void initializationMC() {
+        // MC Pass
+        // MCPasses.add(new Hello());
     }
 
     /**
      * 运行所有IR pass
      */
-    public static void run(Module M){
-        for(Pass p:passes){
-//            System.out.println("IR Pass:"+p.getName());
+    public static void run(Module M) {
+        for (Pass p : passes) {
+            // System.out.println("IR Pass:"+p.getName());
             p.runOnModule(M);
             M.rename();
         }
@@ -55,9 +57,9 @@ public class PassManager {
     /**
      * 运行所有MC pass
      */
-    public static void runMC(CodeGenManager CGM){
-        for(MCPass mp:MCPasses){
-            System.out.println("MC Pass:"+mp.getName());
+    public static void runMC(CodeGenManager CGM) {
+        for (MCPass mp : MCPasses) {
+            System.out.println("MC Pass:" + mp.getName());
             mp.runOnCodeGen(CGM);
         }
     }
