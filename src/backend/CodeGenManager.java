8dbe0b6abcdde15b8752ed0ef53451aaa1536b8a
luxia <linluxia@buaa.edu.cn>
2022-08-09 22:23:33 +0800

    feat: do not save all register on stack

--- src/backend/CodeGenManager.java
+++ src/backend/CodeGenManager.java
@@ -60,14 +60,14 @@ public class CodeGenManager {
         var allocator = new GraphColor(funcList);
         allocator.run();
         var clean = new Clean(funcList);
-//        clean.run();
+        clean.run();
     }
 
     private void halfRun22() {
         var allocator = new RegAllocator(funcList);
         allocator.run();
         var clean = new Clean(funcList);
-//        clean.run();
+        clean.run();
     }
 
     public void run() {
@@ -94,6 +94,8 @@ public class CodeGenManager {
 
         var allocator = new GraphColor(funcList);
         allocator.run();
+        var clean = new Clean(funcList);
+        clean.run();
     }
 
 
@@ -150,7 +152,7 @@ public class CodeGenManager {
         pw2.println(mc.toArm());
         pw2.flush();
 
-        mc.halfRun22();
+        mc.halfRun2();
 
         pw3.println(mc.toArm());
         pw3.flush();
