15106b90dfad9f4a83cf0ce82ad251f5a36ab5f6
luxia <linluxia@buaa.edu.cn>
2022-08-09 19:52:35 +0800

    feat : may be easier to load imm

--- src/backend/machineCode/Instruction/Move.java
+++ src/backend/machineCode/Instruction/Move.java
@@ -11,13 +11,29 @@ import java.util.List;
 public class Move extends MachineInstruction {
     private Register dest;
     private MCOperand op;
+    private Type type;
+
+    public enum Type {
+        MVN,
+        MOV,
+    }
 
     public Move(MachineBasicBlock parent, MCOperand dest, MCOperand op) {
         super(parent);
         assert dest instanceof Register;
         this.dest = (Register) dest;
+        this.type = Type.MOV;
         this.op = op;
-        if(op instanceof Register && ((Register) op).isFloat() || ((Register) dest).isFloat()) setForFloat(true);
+        if (op instanceof Register && ((Register) op).isFloat() || ((Register) dest).isFloat()) setForFloat(true);
+    }
+
+    public Move(MachineBasicBlock parent, Type type, MCOperand dest, MCOperand op) {
+        super(parent);
+        assert dest instanceof Register;
+        this.dest = (Register) dest;
+        this.type = type;
+        this.op = op;
+        if (op instanceof Register && ((Register) op).isFloat() || ((Register) dest).isFloat()) setForFloat(true);
     }
 
     @Override
@@ -48,16 +64,16 @@ public class Move extends MachineInstruction {
     @Override
     public MachineInstruction setForFloat(boolean isForFloat) {
         setForFloat(isForFloat, new ArrayList<>(List.of("32")));
-       return this;
+        return this;
     }
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
         if (isForFloat()) sb.append("v");
-        sb.append("mov");
-        if(getCond() != null) sb.append(getCond());
+        sb.append(type.toString().toLowerCase());
+        if (getCond() != null) sb.append(getCond());
         if (isForFloat()) sb.append(typeInfoString());
-        sb.append( "\t").append(dest.toString() ).append(", ") .append(op);
+        sb.append("\t").append(dest.toString()).append(", ").append(op);
         return sb.toString();
     }
 }
