5764d0c6979fcf9cf2b097093ec7f7785583301e
hribz <1918256943@qq.com>
2022-08-13 10:06:51 +0800

    Loop Unroll

--- src/pass/passes/LoopUnroll.java
+++ src/pass/passes/LoopUnroll.java
@@ -1,13 +1,24 @@
 package pass.passes;
 
+import analysis.DominatorTree;
 import ir.Function;
 import ir.Loop;
 import pass.FunctionPass;
 
+import java.util.ArrayList;
+
 public class LoopUnroll extends FunctionPass {
+    private DominatorTree DT;
+    private ArrayList<Loop> loops;
     @Override
     public void runOnFunction(Function F) {
+        F.getLoopInfo().computeLoopInfo(F);
+        loops=F.getLoopInfo().getAllLoops();
+        if(loops.isEmpty()) return;
+        DT=F.getAndUpdateDominatorTree();
+        for(Loop l:loops){
 
+        }
     }
 
     public void runOnLoop(Loop L){
