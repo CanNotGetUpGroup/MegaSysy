7be596b36b2f69ba105724c469c4d724977ab9bb
hribz <1918256943@qq.com>
2022-08-16 16:43:12 +0800

    test

--- src/Compiler.java
+++ src/Compiler.java
@@ -24,7 +24,7 @@ public class Compiler {
         FileWriter fw=new FileWriter(args[2]);
         PrintWriter pw=new PrintWriter(fw);
 
-        String[] str={""};
+        String[] str={};
         for(String s:str){
             if(args[3].endsWith(s)){
 //                InputStream in=new FileInputStream(args[3]);
