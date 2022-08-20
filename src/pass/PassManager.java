e1fc5374bbb7a5c590a8cd82d2aa7815d8c70994
Horik <hr.mail.qaq@gmail.com>
2022-08-20 18:01:46 +0800

    [fix] find Input call stackoverflow err

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -13,7 +13,7 @@ public class PassManager {
     public static ArrayList<MCPass> MCPasses = new ArrayList<>();
     // 官方测例可以保证不会出现undef的情况，因此某些情况下可以激进地忽略掉undef（可能无法通过中端测试）
     public static boolean ignoreUndef = false;
-    public static boolean eliminatePreHeader = true;
+    public static boolean eliminatePreHeader = false;
     public static boolean aggressive = false;
     public static boolean debug = true;
 
@@ -49,7 +49,7 @@ public class PassManager {
         passes.add(new LoopInfoUpdate());
         passes.add(new LICM());// 循环不变量外提
         passes.add(new LCSSA());
-        passes.add(new LoopFusion());
+//        passes.add(new LoopFusion());
         passes.add(new SimplifyCFG(eliminatePreHeader));
 
         passes.add(new GVNGCM(aggressive));
@@ -60,15 +60,15 @@ public class PassManager {
         passes.add(new LocalArrayPromote());
         passes.add(new InterProceduralDCE());
         passes.add(new GlobalVariableOpt());
-//         passes.add(new LoopUnroll(false));//还存在bug，开了也不知道能不能快，干脆不开了
-//         passes.add(new LoopUnroll(false));
+        // passes.add(new LoopUnroll(false));//还存在bug，开了也不知道能不能快，干脆不开了
+        // passes.add(new LoopUnroll(false));
         passes.add(new LoopRedundant());
 
         aggressive = true;// 激进的GVN，消除掉数组参数的alloca，并关闭ArraySSA
         eliminatePreHeader = true;// 完成了循环优化，删掉preHeader
         passes.add(new SimplifyCFG(eliminatePreHeader));
         passes.add(new GVNGCM(aggressive));
-        passes.add(new EliminateDimInfo());//消除掉DimInfo可能对GVNGCM造成的影响
+        passes.add(new EliminateDimInfo());// 消除掉DimInfo可能对GVNGCM造成的影响
         passes.add(new GVNGCM(aggressive));
         passes.add(new EliminateAlloca());// 由于GVN需要使用alloca，因此最后再删除
         if (debug) {
