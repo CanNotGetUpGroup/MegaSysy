23a0c242b3b9adca1f38393e34b46876877c66ed
hribz <1918256943@qq.com>
2022-08-20 18:28:14 +0800

    test error

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -41,9 +41,9 @@ public class PassManager {
 
         passes.add(new LCSSA());
         passes.add(new LoopInfoUpdate()); // 计算循环信息
-        passes.add(new IndVarReduction());
+//        passes.add(new IndVarReduction());
         passes.add(new LICM());// 循环不变量外提
-//        passes.add(new InterProceduralDCE());
+        passes.add(new InterProceduralDCE());
         passes.add(new GVNGCM(aggressive));
         passes.add(new LoopRedundant());
 
@@ -51,7 +51,7 @@ public class PassManager {
         passes.add(new GlobalVariableOpt());// FuncInline为其创造更多机会
         passes.add(new Mem2Reg());// 处理掉新产生的alloca
         passes.add(new FuncInline());// 可能还有
-//        passes.add(new InterProceduralDCE());
+        passes.add(new InterProceduralDCE());
         passes.add(new LoopInfoUpdate());
         passes.add(new LICM());// 循环不变量外提
         // passes.add(new LCSSA());
@@ -64,11 +64,14 @@ public class PassManager {
         passes.add(new SimplifyCFG(eliminatePreHeader));
         passes.add(new LoopUnroll(true));// 常量循环消除
         passes.add(new LocalArrayPromote());
-//        passes.add(new InterProceduralDCE());
+        passes.add(new InterProceduralDCE());
         passes.add(new GlobalVariableOpt());
         // passes.add(new LoopUnroll(false));//还存在bug，开了也不知道能不能快，干脆不开了
         // passes.add(new LoopUnroll(false));
         passes.add(new LoopRedundant());
+        if (debug) {
+            passes.add(new VerifyFunction());
+        }
 
         aggressive = true;// 激进的GVN，消除掉数组参数的alloca，并关闭ArraySSA
         eliminatePreHeader = true;// 完成了循环优化，删掉preHeader
@@ -77,9 +80,6 @@ public class PassManager {
         passes.add(new EliminateDimInfo());// 消除掉DimInfo可能对GVNGCM造成的影响
         passes.add(new GVNGCM(aggressive));
         passes.add(new EliminateAlloca());// 由于GVN需要使用alloca，因此最后再删除
-        if (debug) {
-            passes.add(new VerifyFunction());
-        }
     }
 
     public static void functionalOpt() {
